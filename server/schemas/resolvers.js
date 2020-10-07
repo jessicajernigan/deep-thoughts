9bddd1801befa27afb891add150eb8bbbce1fcde
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-10-07 17:08:21 -0500

    lesson 1

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,30 @@
+const { User, Thought } = require('../models');
+
+const resolvers = {
+  Query: {
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    // place this inside of the `Query` nested object right after `thoughts` 
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+    // get all users
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+  } //"5f7e36b9c69f8a33a400d849"
+};
+
+module.exports = resolvers;
