d7656be34df78ed452ca667f5db82649095c3f73
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-10-09 19:47:59 -0500

    end of lesson 4

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,14 @@ import { ApolloProvider } from '@apollo/react-hooks'; // a special type of React
 import ApolloClient from 'apollo-boost'; // get that data when we're ready to use it
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 
 const client = new ApolloClient({
   uri: '/graphql' // URI stands for "Uniform Resource Identifier."
@@ -14,15 +20,25 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
+
 
 export default App;
